8d1a4c6a0865a5882257223644598a859b6eed7d
arjunkakkar8 <arjunkakkar8@gmail.com>
2024-04-05 15:11:37 -0700

    Updated query parsing

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use polars::prelude::*;
+use actix_web::{get, http::header::ContentType, web, App, HttpResponse, HttpServer, Responder};
+use polars::{
+    lazy::dsl::{concat_str, Expr},
+    prelude::*,
+};
 use serde::Deserialize;
 use std::collections::HashMap;
 
 #[derive(Deserialize, Debug)]
 #[serde(untagged)]
 enum UorV {
-    U(u8),
-    V(Vec<u8>),
+    U(f32),
+    V(Vec<f32>),
 }
 
 #[derive(Deserialize, Debug)]
@@ -22,42 +25,111 @@ struct Query {
     age_max: u8,
 }
 
-struct AppState {
-    data: DataFrame,
+fn construct_filter(
+    select: &String,
+    where_clause: &HashMap<String, UorV>,
+    group_by_clause: &Vec<String>,
+) -> Expr {
+    let mut expr: Expr = col(&select).neq(-1);
+    for (key, value) in where_clause.iter() {
+        match value {
+            UorV::U(val) => {
+                expr = expr.and(col(key).eq(*val));
+            }
+            UorV::V(vals) => {
+                let mut local_expr: Expr = col(key).eq(vals[0]);
+                for val in vals.iter().skip(1) {
+                    local_expr = local_expr.or(col(key).eq(*val));
+                }
+                expr = expr.and(local_expr);
+            }
+        }
+    }
+
+    for group_by in group_by_clause.iter() {
+        expr = expr.and(col(group_by).neq(-1));
+    }
+
+    expr
+}
+
+fn construct_group_by(select: &String, group_by: &Vec<String>) -> Vec<Expr> {
+    let mut exprs: Vec<Expr> = Vec::new();
+    exprs.push(col(&select));
+
+    for group_by in group_by.iter() {
+        exprs.push(col(group_by));
+    }
+
+    exprs
+}
+
+// Consider renaming the original data columns to avoid this step
+fn reformat_group_by(group_by: &Vec<String>) -> Vec<String> {
+    let mut new_group_by: Vec<String> = Vec::new();
+    for group_by in group_by.iter() {
+        match group_by.as_str() {
+            "sex" => {
+                new_group_by.push("SEX".to_string());
+            }
+            _ => {
+                new_group_by.push(group_by.to_string());
+            }
+        }
+    }
+
+    new_group_by
 }
 
 #[get("/")]
 async fn get_data(query: web::Json<Query>) -> impl Responder {
-    println!("Query Recieved");
     let args = ScanArgsParquet::default();
     let lf = LazyFrame::scan_parquet("data/processed_2022.parquet", args).unwrap();
-    let select = query.select.first().unwrap();
+    let select_string = query.select.first().unwrap();
+    let group_by = reformat_group_by(&query.group_by);
     let computed = lf
-        .filter(
-            col(&select)
-                .neq(-1)
-                .and(col("latino_race").neq(-1))
-                .and(col("SEX").neq(-1))
-                .and(col("veteran").neq(-1)),
+        .filter(construct_filter(&select_string, &query.r#where, &group_by))
+        .cast_all(DataType::Int32, true)
+        .group_by(&construct_group_by(&select_string, &group_by))
+        .agg(&[col("PERWT").sum().alias("count")])
+        .with_columns([concat_str(
+            &[lit((*select_string).as_str()), col(&select_string)],
+            "_",
+            false,
         )
-        .group_by([col(&select), col("SEX"), col("latino_race"), col("veteran")])
-        .agg([col("PERWT").sum().alias("count")])
+        .alias("citizenship")])
         .collect()
         .unwrap();
+
     println!("Data {:?} \n\n Query {:?}", computed, query);
-    HttpResponse::Ok().body("Hello world!")
+
+    let pivoted = pivot::pivot_stable(
+        &computed,
+        group_by,
+        [select_string],
+        Some(["count"]),
+        false,
+        None,
+        None,
+    )
+    .unwrap();
+
+    let mut buffer = Vec::new();
+
+    JsonWriter::new(&mut buffer)
+        .with_json_format(JsonFormat::Json)
+        .finish(&mut pivoted.clone())
+        .unwrap();
+
+    let json_string = String::from_utf8(buffer).unwrap();
+
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(json_string)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // let mut data_file = std::fs::File::open("data/processed_2022.parquet").unwrap();
-    //
-    // let data = web::Data::new(AppState {
-    //     data: ParquetReader::new(&mut data_file).finish().unwrap(),
-    // });
-    //
-    // .app_data(data.clone())
-
     HttpServer::new(move || App::new().service(get_data))
         .bind(("127.0.0.1", 8080))?
         .run()
